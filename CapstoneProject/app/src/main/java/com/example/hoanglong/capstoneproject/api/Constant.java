2698e9072d6c1abd48a35a918191893f53860480
LongPham <long.phamlp94@gmail.com>
2018-01-21 01:06:10 +0700

    initial commit
    initial commit

---
+++ CapstoneProject/app/src/main/java/com/example/hoanglong/capstoneproject/api/Constant.java
@@ -0,0 +1,10 @@
+package com.example.hoanglong.capstoneproject.api;
+
+/**
+ * Created by hoanglong on 10/08/2016.
+ */
+
+public class Constant {
+    public static final String MAP_URL = "https://maps.googleapis.com/maps/";
+//    public static final String WEB_URL = "http://9791174a.ngrok.io/";
+}
