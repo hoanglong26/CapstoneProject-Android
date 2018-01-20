851ea99c48ece08ea0796280f212edadde2d4dfe
LongPham <long.phamlp94@gmail.com>
2018-01-21 01:27:13 +0700

    add api folder

---
+++ CapstoneFPT/app/src/main/java/com/example/hoanglong/capstonefpt/api/Constant.java
@@ -0,0 +1,10 @@
+package com.example.hoanglong.capstonefpt.api;
+
+/**
+ * Created by hoanglong on 10/08/2016.
+ */
+
+public class Constant {
+    public static final String MAP_URL = "https://maps.googleapis.com/maps/";
+//    public static final String WEB_URL = "http://9791174a.ngrok.io/";
+}
